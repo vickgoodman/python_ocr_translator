8d2dc2d8a2ff270ef8ed7e3be28c9667f3607540
Sava Victor <victor.sava.00@gmail.com>
2025-08-19 20:57:03 +0300

    Minor changes to config files

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
-SOURCE_USERNAME = 'liviumailer'
+SOURCE_USERNAME = 'minte.motivata'
 TARGET_USERNAME = 'motivated.mindset'
 
 DEVELOPER_PROMPT = 'You are a helpful translator assistant. ' \
 'Respond only with the translated text. ' \
-'Try to have as many words as the original text.'
+'Try to have the same number of words as the original text.'
